d8c8d4fc87b8659523f6d449b11b2349393dfa27
umaircloud57 <umaircloud57@gmail.com>
2026-02-23 15:20:21 +0500

    data types

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+price = 40
+salary = 400.45
+name = "umair"
+items = ["apple", "book","laptop","chair"]
+static_items = ("09","1256",36)
+user_info = {'name':"hamza",'age':33,"address":" multan "}
+values = {12,224,45,10,12}
+
+
+print(type(values))
+ 
+
+
+
+def sum(val1,val2):
+    return val1 + val2
+
+value1 = 50
+value2 = 10
+sum1 = value1 + value2
+print( sum1)
+
+
+print(sum(30,100))
