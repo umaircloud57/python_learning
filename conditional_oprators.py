78b7a8ee6c968dad2250ea42d78e027d60ffd348
umaircloud57 <umaircloud57@gmail.com>
2026-02-26 14:01:21 +0500

    new files added

---
+++ conditional_oprators.py
@@ -0,0 +1,25 @@
+# a = int(input("ENTER THE NUMBER "))
+# b = int(input("enter 2nd number "))
+# if a > b:
+#     print("the number is greater : ",a) 
+
+
+
+print(10 < 3)
+
+print(4 == 4)
+
+# print(2!=5)
+
+age = int (input("enter your age "))
+if age >= 18:
+    print("You are eligible to vote.") 
+else:
+    print("you're are not eligible to vote ")
+# num1=int(input("enter a number "))
+# if(num1%2)==0:
+#     print('the number is even')
+# else:
+#     print("the number is odd ")    
+ 
+ 
