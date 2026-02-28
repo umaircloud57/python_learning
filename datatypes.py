dc62b23499a3f88a1085f4a957e193e6839c1364
umaircloud57 <umaircloud57@gmail.com>
2026-02-28 15:55:15 +0500

    edit

--- datatypes.py
+++ datatypes.py
@@ -1,21 +1,32 @@
-price = 40
-salary = 400.45
-name = "umair"
+# price = 40
+# salary = 400.45
+# name = "umair"
+# items = ["apple", "book","laptop","chair"]
+# static_items = ("09","1256",36)
+# user_info = {'name':"hamza",'age':33,"address":" multan "}
+# values = {12,224,45,10,12}
+
+
+# print(type(values))
+ 
+
+
+
+
+
+
+ 
+# print(sum([2,30,40,50,500,1000,2300,10,]))
+
+# print(sum([10,202,300]))
+
 items = ["apple", "book","laptop","chair"]
-static_items = ("09","1256",36)
-user_info = {'name':"hamza",'age':33,"address":" multan "}
-values = {12,224,45,10,12}
+items.extend(["peach","grapes", "orange"])
+print(items[::])
+print(len(items))
 
 
-print(type(values))
- 
+# nums = [4, 7, 2, 9]
 
-
-
-
-
-
- 
-print(sum([2,30,40,50,500,1000,2300,10,]))
-
-print(sum([10,202,300]))
+# nums.extend([2,52,54])
+# print(nums)
--- if_else.py
+++ if_else.py
@@ -1,1 +1,29 @@
-print("we are learning python ")
+age= int(input("enter your age "))
+if age >= 18:
+    print("you're eligible to vote for country ")
+else :
+    print("you're not eligible for vote for country")
+
+
+user_marks=int(input("enter you're marks"))
+if user_marks>=60:
+    print("congrulations you passed this exams")
+else:
+    print("you failed this exams")
+
+
+
+# avail_list = ["apple","banana", "peach", 'graphes' , 'orange', 'mango']
+# srch_list = "apple"
+# if avail_list in srch_list:
+#     print)
+
+
+
+
+
+
+
+
+
+
