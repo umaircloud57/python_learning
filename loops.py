28d7f1cc937e03e72b20306e00471fd62b491c5f
umaircloud57 <umaircloud57@gmail.com>
2026-03-06 14:44:19 +0500

    loop in python

---
+++ loops.py
@@ -0,0 +1,19 @@
+# list=[1,32,5,5,6,]
+# for list in list :
+#     print(list)
+
+
+data=("ali,ahmed,")
+for data in data :
+    print (data)
+
+
+
+
+
+# for nums in [12,2, 5,2,6]:
+#     print(nums)
+
+
+for characters in "we love pakistan " :
+    print(characters)
